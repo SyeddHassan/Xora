9f3e92c1aa8269b1c2b7f58eee98ae4a4885ac93
SyeddHassan <syeddhassanali@gmail.com>
2024-09-25 18:50:00 -0700

    implementation of hero section along with button and marker component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
-import Header from "./sections/header";
+import Header from "./sections/header-section";
+import Hero from "./sections/hero-section";
 
 const App = () => {
   return (
     <main className="overflow-hidden">
       <Header />
+      <Hero />
     </main>
   );
 };
