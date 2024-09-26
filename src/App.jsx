abaeacf8482be934d214c39dc828ae813f984348
SyeddHassan <syeddhassanali@gmail.com>
2024-09-25 18:57:28 -0700

    implement features section

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Header from "./sections/header-section";
 import Hero from "./sections/hero-section";
+import Features from "./sections/features-section";
 
 const App = () => {
   return (
     <main className="overflow-hidden">
       <Header />
       <Hero />
+      <Features />
     </main>
   );
 };
